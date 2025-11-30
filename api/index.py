a8bcfe081bd050a43567deee79b379dca63d02ce
sf-yuzifu <yuzifu@yuzifu.top>
2025-11-30 16:39:53 +0800

    [pref]修改请求头

--- api/index.py
+++ api/index.py
@@ -508,10 +508,10 @@ def get_comic_info(comic_id: str):
         return jsonify({"error": str(e)}), 500
 
 
-@app.route("/comic/<comic_id>/chapter/<int:chapter_number>", methods=["GET", "POST"])
+@app.route("/comic/<comic_id>/chapter/<int:chapter_number>")
 def get_specific_chapter(comic_id: str, chapter_number: int):
     """获取特定章节信息"""
-    cookie = request.json.get("cookie") if request.is_json else None
+    cookie = request.headers.get("QQComic-Cookie") if request.headers.get("QQComic-Cookie") else None
     try:
         comic_info = ComicParser.get_comic_info(comic_id)
         if "error" in comic_info:
